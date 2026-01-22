98cd4be09535e264c3226eee9e42dc6a155f83bb
Naleper90 <esanatyoe@gmail.com>
2026-01-22 18:49:03 +0100

    Fix URL en environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // MAÑANA pondrás aquí tu URL de Railway. Por ahora déjalo así o pon una dummy.
-  apiUrl: 'korporativo-vinilos-production.up.railway.app'
+  apiUrl: 'https://korporativo-vinilos-production.up.railway.app/api'
 };
