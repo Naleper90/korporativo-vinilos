e293fbe9afe22b157e35d21231fd71af95bd6280
Naleper90 <esanatyoe@gmail.com>
2026-01-22 21:29:43 +0100

    Despliegue con Base de datos PostgreSQL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // MAÑANA pondrás aquí tu URL de Railway. Por ahora déjalo así o pon una dummy.
   apiUrl: 'https://korporativo-vinilos-production.up.railway.app/api'
 };
