1da75a41bca1847c7eb17bb80dd968a725769816
Naleper90 <esanatyoe@gmail.com>
2026-01-22 18:21:25 +0100

    Ajustando despliegue

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // MAÑANA pondrás aquí tu URL de Railway. Por ahora déjalo así o pon una dummy.
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'korporativo-vinilos-production.up.railway.app'
 };
